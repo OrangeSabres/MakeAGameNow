5b22575cb5d602790bb444892c30bb99ce168881
JEFF\OrangeSabres <jefferyrshaffer@gmail.com>
2018-02-28 14:59:43 -0500

    Initial commit

---
+++ Assets/MoveLandR.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLandR : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+	}
+}
